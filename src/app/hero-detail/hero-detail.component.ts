fe6a9c7deebe8f83e06605a80ab0e7c4ea49818f
Olga Osadchaya <olgaosadchaya02@mail.ru>
2022-04-13 22:47:33 +0300

    Change routing

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -4,23 +4,35 @@ import { Location } from '@angular/common';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { Router } from '@angular/router'
+
+
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: [ './hero-detail.component.css' ]
+  styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero | undefined;
+  viewMode: boolean = true;
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
-    private location: Location
-  ) {}
+    private location: Location,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.getHero();
+    var param = this.route.snapshot.paramMap.get('id');
+    if (param) {
+      this.getHero();
+      this.viewMode = true;
+    } else {
+      this.hero = {} as Hero;
+      this.viewMode = false;
+    }
   }
 
   getHero(): void {
@@ -35,9 +47,21 @@ export class HeroDetailComponent implements OnInit {
 
   save(): void {
     if (this.hero) {
-      this.heroService.updateHero(this.hero)
-        .subscribe(() => this.goBack());
+      if (this.hero.id) {
+        this.heroService.updateHero(this.hero)
+          .subscribe(() => this.goBack());
+      } else {
+        this.heroService.addHero(this.hero)
+          .subscribe(hero => {
+            this.router.navigateByUrl('/detail/' + hero.id);
+          });
+      }
     }
+    this.viewMode = true;
   }
-  
+
+  update(): void {
+    this.viewMode = !this.viewMode;
+  }
+
 }
